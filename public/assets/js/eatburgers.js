c47a98bf8064bb46a3ec5c9adf3a55768cafb913
jessica <papylotomba@yahoo.fr>
2020-05-05 19:24:32 -0400

    styling and coding eatburgers.js

---
+++ public/assets/js/eatburgers.js
@@ -0,0 +1,41 @@
+$(function() {
+    // if the devoured button (uneaten-li-btn) was clicked,
+    // change it to eaten in the database and on the website.
+    $(".uneaten-li-btn").on("click", function(event) {
+      const id = $(this).data("id");
+  
+      // make it devoured (eaten)
+      const devouredTrue = {
+        devoured: true
+      };
+  
+      // Send the PUT request by id
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredTrue
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  
+    // when submit button hit, add a new burger to the db & webpage
+    $(".new-burger-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      // get new burger name from the website
+      const newBurger = {
+        new_burger: $("#burg").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  });
